3bca7e691b27df1fe4c58ea7aa4e0b7c48436ded
NatalieM871 <natalie_m118@yahoo.com>
2024-07-30 23:03:20 +1000

    Add files via upload

---
+++ task1.ру.py
@@ -0,0 +1,8 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print(9.99 > 9.98, 1000 != 1000.1)
+# 3rd program
+print(((1234 % 1000 - 4) // 10) + ((5678 % 5000 - 8) //10))
+# 4th program
+print((int(42.13) == round(13.42 % 13, 2) * 100) or (int(13.42) == round(42.13 % 42, 2) * 100))
